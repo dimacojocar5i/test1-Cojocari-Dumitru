8cd50dd9d55fc5583ed18febcfefda97f01e07db
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-06-08 23:32:37 +0300

    Add new class Ex11.java

---
+++ src/Ex11.java
@@ -0,0 +1,8 @@
+public class Ex11 {
+    static void main(String[] args) {
+        String text = "Diana bate capu";
+        int raspuns = text.length() - 1;
+        System.out.println("Indicile maxim este: " + raspuns);
+        System.out.println("Cantitatea de caractere: " + text.length());
+    }
+}
