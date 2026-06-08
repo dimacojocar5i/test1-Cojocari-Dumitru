faeada27df50e39c15b73bd1f53c1d104be3a337
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-06-08 23:37:11 +0300

    Add new class Ex16.java

---
+++ src/Ex16.java
@@ -0,0 +1,10 @@
+public class Ex16 {
+    static void main(String[] args) {
+        int [] array = {45, 9, 8, 3, 56, 78, 96, 32, 4, 61};
+
+        System.out.println("Valoarea primului element: " + array[0]);
+        int value = array[array.length - 1];
+        System.out.println("Valoarea ultimului element: " + value);
+        System.out.println("Lungimea array-lui este: " + array.length);
+    }
+}
