d29f19e6c62a4e7e9c037905598925c13373c228
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-06-08 23:25:07 +0300

    Add new class Ex4.java

---
+++ src/Ex4.java
@@ -0,0 +1,13 @@
+public class Ex4 {
+    static void main(String[] args) {
+
+        int number1 = 45;
+        int number2 = 12;
+        if (number1 > number2) {
+            System.out.println("Primul numar este mai mare!");
+        }
+        else {
+            System.out.println("Al doilea numar este mai mare!");
+        }
+    }
+}
