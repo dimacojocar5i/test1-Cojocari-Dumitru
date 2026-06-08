f1460f681fcc09d1bbb34958fc64b7749aaea6f6
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-06-08 23:44:35 +0300

    Modified class Ex11.java

--- src/Ex11.java
+++ src/Ex11.java
@@ -1,6 +1,6 @@
 public class Ex11 {
     static void main(String[] args) {
-        String text = "Diana bate capu";
+        String text = "Diana face mincare";
         int raspuns = text.length() - 1;
         System.out.println("Indicile maxim este: " + raspuns);
         System.out.println("Cantitatea de caractere: " + text.length());
