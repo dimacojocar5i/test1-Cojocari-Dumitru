9ec5f240917bea1e6482088c29cee4e1bfca42e8
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-06-08 14:18:33 +0300

    Add new class Ex1.java

---
+++ src/Ex1.java
@@ -0,0 +1,16 @@
+public class Ex1 {
+    static void main(String[] args) {
+        int result1 = - 6 + 8 * 6;
+        double result2 = (44 + 8) % 5;
+        double result3 = 19 + - (double) 3 * 2 / 8;
+        double result4 = ((5 +(double) 20 / 3 * 2) - 8) % 4;
+
+        System.out.println("Rezultatul primului exercitiu: " + result1);
+        System.out.println("Rezultatul la al doilea exercitiu: " + result2);
+        System.out.println("Rezultatul la al treilea exercitiu: " + result3);
+        System.out.printf( "Rezultatul la al patrulea exercitiu: " + "%.2f", result4);
+
+
+
+    }
+}
