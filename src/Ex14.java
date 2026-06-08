2343f8676baf81098aa057ec03e189502aaaf52c
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-06-08 23:35:34 +0300

    Add new class Ex14.java

---
+++ src/Ex14.java
@@ -0,0 +1,11 @@
+public class Ex14 {
+    static void main(String[] args) {
+
+        for (int i = 0; i <= 20; i++) {
+            if (i == 14) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
